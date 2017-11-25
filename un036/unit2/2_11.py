f8f159e36e0a10faac77e5b999077e4bd5086b6d
thinkreed <thinkreed2017@outlook.com>
2017-11-25 21:02:56 +0800

    add ud036 unit 2 1

---
+++ un036/unit2/2_11.py
@@ -0,0 +1,6 @@
+import webbrowser
+import time
+
+for i in range(0, 3):
+    time.sleep(5)
+    webbrowser.open('http://www.google.com')
---
+++ un036/unit2/2_27.py
@@ -0,0 +1,22 @@
+import os
+from string import digits
+
+def rename_files_in_dir():
+    files_list = os.listdir(r'/Users/huweijie/Downloads/prank')
+    print(files_list)
+    current_path = os.getcwd()
+    os.chdir(r'/Users/huweijie/Downloads/prank')
+    remove_digits = str.maketrans('', '', digits)
+    for file_name in files_list:
+        os.rename(file_name, file_name.translate(remove_digits))
+    os.chdir(current_path)
+
+
+def test():
+    rename_files_in_dir()
+    test_str = '2353587jjjjh4455'
+    remove_digits = str.maketrans('', '', digits)
+    print(test_str.translate(remove_digits))
+
+
+test()
