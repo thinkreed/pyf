39c387aa07c12f47a4328f4befb4142c3cf9750b
thinkreed <thinkreed2017@outlook.com>
2017-12-11 22:06:49 +0800

    add crawler parse all href

--- crawler/main.py
+++ crawler/main.py
@@ -1,10 +1,20 @@
 import requests
+from bs4 import BeautifulSoup
+from threading import Thread
+
+
+def get_all_a_tag(response):
+    soup = BeautifulSoup(response.text)
+    for a_tag in soup.find_all('a'):
+        print(a_tag['href'])
 
 
 def main():
-    content = requests.get('https://www.baidu.com')
-    index = content.content.find('<h2')
-    print(index)
+    destination_url = 'https://www.qu.la'
+    response = requests.get(destination_url)
+    parse_thread = Thread(target=get_all_a_tag, args=(response,))
+    parse_thread.start()
+    parse_thread.join()
 
 
 main()
