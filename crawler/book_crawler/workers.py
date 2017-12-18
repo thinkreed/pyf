ce6153fdedb7d030a54bf7786579863d23e772fc
thinkreed <thinkreed2017@outlook.com>
2017-12-18 21:37:33 +0800

    add server to serve http get

--- crawler/book_crawler/workers.py
+++
@@ -1,9 +0,0 @@
-from concurrent.futures import ThreadPoolExecutor
-
-
-class Workers:
-    def __init__(self):
-        self.executor = ThreadPoolExecutor(max_workers=4)
-
-    def get_worker(self):
-        return self.executor
