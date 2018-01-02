8e35db5751c0fd067a799650e82403df16439316
thinkreed <thinkreed2017@outlook.com>
2018-01-02 21:22:54 +0800

    use response follow to yield requests

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -16,7 +16,5 @@ class QuotesSpider(scrapy.Spider):
                 'author': quote.css('small.author::text').extract_first(),
                 'tags': quote.css('div.tags a.tag::text').extract()
             }
-        next_page = response.css('li.next a::attr(href)').extract_first()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+        for a in response.css('li.next a'):
+            yield response.follow(a, callback=self.parse)
