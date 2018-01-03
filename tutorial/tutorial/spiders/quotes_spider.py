a1d9a98137bcf70a58e1688d1a9c9f5a3434a0ed
thinkreed <thinkreed2017@outlook.com>
2018-01-03 20:53:19 +0800

    scrapy step 4

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -4,10 +4,12 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
 
-    start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
-    ]
+    def start_requests(self):
+        url = 'http://quotes.toscrape.com/'
+        tag = getattr(self, 'tag', None)
+        if tag is not None:
+            url = url + 'tag/' + tag
+        yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
         for quote in response.css('div.quote'):
