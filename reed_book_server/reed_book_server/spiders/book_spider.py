2eef506b9816afa6370b1efad0b9be0e902993db
thinkreed <thinkreed2017@outlook.com>
2018-01-05 20:44:15 +0800

    scrapy step 7

--- reed_book_server/reed_book_server/spiders/book_spider.py
+++ reed_book_server/reed_book_server/spiders/book_spider.py
@@ -5,12 +5,12 @@ from reed_book_server.items import ReedBookServerItem
 
 
 class BookSpider(CrawlSpider):
-
     name = "biqu.la"
-    start_urls = ['https://www.biqu.la/']
+    start_urls = ['https://www.biqu.la/book/']
 
     rules = (
-        Rule(LinkExtractor(deny=('/author')), callback='parse_item'),
+        Rule(LinkExtractor(allow=('/book/0-default'))),
+        Rule(LinkExtractor(allow=('/book/[A-Z]'), deny=('/author')), callback='parse_item'),
     )
 
     def parse_item(self, response):
