27c2a683ada278e9faacbf39ca62822e40bc34db
thinkreed <thinkreed2017@outlook.com>
2018-01-08 20:33:57 +0800

    add book with chapter index

--- reed_book_server/reed_book_server/spiders/book_spider.py
+++ reed_book_server/reed_book_server/spiders/book_spider.py
@@ -1,6 +1,11 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from reed_book_server.items import ReedBookServerItem
+from scrapy import Selector
+
+
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
 
 class BookSpider(CrawlSpider):
@@ -17,5 +22,17 @@ class BookSpider(CrawlSpider):
         item['book_name'] = response.xpath("//div[@id='info']//h1/text()").extract()[0]
         item['author_name'] = response.xpath("//span[@class='item item']//a/text()").extract()[0]
         item['book_sites'] = {}
-        item['book_sites']['笔趣阁'] = response.url
+        item['book_sites']['name'] = '笔趣阁'
+        item['book_sites']['chapters'] = []
+        i = 0
+        for chunk in chunker(response.xpath("//div[@class='book_list']//ul//li//a").extract(), 4):
+            for a_tag in reversed(chunk):
+                a_selector = Selector(text=a_tag)
+                chapter = {}
+                chapter['index'] = i
+                chapter['url'] = response.url + a_selector.xpath("//a/@href").extract_first()
+                chapter['title'] = a_selector.xpath("//a//text()").extract_first()
+                item['book_sites']['chapters'].append(chapter)
+                i += 1
+
         return item
