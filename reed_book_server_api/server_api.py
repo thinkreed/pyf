dd87a51898aa41a1d9e102311af680145588bf0b
thinkreed <thinkreed2017@outlook.com>
2018-01-10 20:50:58 +0800

    add book api

--- reed_book_server_api/server_api.py
+++ reed_book_server_api/server_api.py
@@ -1,7 +1,11 @@
 from flask import request, url_for
 from flask_api import FlaskAPI, status, exceptions
+from flask_pymongo import PyMongo
 
 app = FlaskAPI(__name__)
+
+app.config['MONGO_DBNAME'] = 'reed_book_server'
+mongo = PyMongo(app)
 
 
 @app.route("/<book_name>")
@@ -9,9 +13,10 @@ def book_indices(book_name):
     pass
 
 
-@app.route("/<book_name>/<int:index>")
-def book_chapter(book_name, index):
-    pass
+@app.route("/book/", methods=['POST', 'GET'])
+def book_chapter():
+    book = mongo.db.books.find_one_or_404({'book_name': str(request.data.get('book_name', ''))})
+    return book['book_sites']['chapters'][int(request.data.get('chapter_index', 0))]
 
 
 if __name__ == "__main__":
