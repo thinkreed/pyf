c0bcf442977fec57cd6f6b539eabefbb2d713db5
thinkreed <thinkreed2017@outlook.com>
2018-01-20 15:55:57 +0800

    add leetcode problem 3 length of longest substring

---
+++ algo/p3_length_of_longest_substring.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        pass_dict = {}
+        longest_length = substring_start = 0
+        for i, c in enumerate(s):
+            if c in pass_dict and substring_start < pass_dict[c]:
+                substring_start = pass_dict[c] + 1
+            else:
+                longest_length = max(longest_length, i - substring_start + 1)
+            pass_dict[c] = i
+
+        return longest_length
