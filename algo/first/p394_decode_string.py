af1c7e970ee8a36f20075e00bc276de47004f43c
thinkreed <thinkreed2017@outlook.com>
2018-04-08 13:32:25 +0800

    fix problem 394 decode string

--- algo/first/p394_decode_string.py
+++ algo/first/p394_decode_string.py
@@ -1,33 +1,36 @@
 class Solution:
+
+    def __init__(self):
+        self.i = 0
+
     def decodeString(self, s):
         """
         :type s: str
         :rtype: str
         """
 
-        def helper(s, i):
+        self.i = 0
+
+        def helper(s):
             res = ''
+            n = 0
 
-            while i < len(s):
-                if not s[i].isdigit():
-                    res += s[i]
-                    i += 1
-                elif s[i].isdigit():
-                    n = 0
-                    while i < len(s) and s[i].isdigit():
-                        n = n * 10 + ord(s[i]) - ord('0')
-                        i += 1
-
-                    i += 1
-                    extracted = helper(s, i)
-
+            while self.i < len(s):
+                if s[self.i].isdigit():
+                    while s[self.i].isdigit():
+                        n = n * 10 + ord(s[self.i]) - ord('0')
+                        self.i += 1
+                    self.i += 1
+                    extracted = helper(s)
                     while n > 0:
                         res += extracted
                         n -= 1
-                elif s[i] == ']':
-                    i += 1
+                elif s[self.i].isalpha():
+                    res += s[self.i]
+                    self.i += 1
+                elif s[self.i] == ']':
+                    self.i += 1
                     return res
 
-        return helper(s, 0)
-
-print(Solution().decodeString("3[a2[c]]"))
+            return res
+        return helper(s)
