c697ce6b2d05c9f8ae1c9f2106e0213c450b59db
thinkreed <thinkreed2017@outlook.com>
2018-04-11 22:55:57 +0800

    add problem 718 maximum length of repeated subarray

---
+++ algo/first/p718_maximum_length_of_repeated_subarray.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findLength(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: int
+        """
+        dp = [x[:] for x in [[0] * 10] * 10]
+
+        for i in range(1, len(A) + 1):
+            for j in range(1, len(B) + 1):
+                if A[i - 1] == B[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+
+        return max(max(vals) for vals in dp)
