b81ba549fedc7b79607c4f3ec326cf5e2680e239
thinkreed <thinkreed2017@outlook.com>
2018-05-08 07:21:27 +0800

    add problem 73 set matrix zeros

---
+++ algo/first/p73_set_matrix_zeros.py
@@ -0,0 +1,23 @@
+class Solution:
+    def setZeroes(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: void Do not return anything, modify matrix in-place instead.
+        """
+        column_state = 1
+        m = len(matrix)
+        n = len(matrix[0])
+
+        for i in range(m):
+            if matrix[i][0] == 0:
+                column_state = 0
+            for j in range(1, n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = matrix[0][j] = 0
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, 0, -1):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+            if column_state == 0:
+                matrix[i][0] = 0
