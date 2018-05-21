081fc79553b72e0f37a63ab9a0a2013d68fe3c1c
thinkreed <thinkreed2017@outlook.com>
2018-05-22 06:53:37 +0800

    add problem 201 bitwise and of number ranges

---
+++ algo/first/p201_bitwise_and_of_number_ranges.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rangeBitwiseAnd(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        while n > m:
+            n = n & (n - 1)
+
+        return n
