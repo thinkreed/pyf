99c0b9ae2eba0694ecc28ef8f504aff47c8fd92b
thinkreed <thinkreed2017@outlook.com>
2018-05-24 07:49:19 +0800

    amend to problem 368 largest divisible subset

--- algo/first/p368_largest_divisible_subset.py
+++ algo/first/p368_largest_divisible_subset.py
@@ -10,3 +10,27 @@ class Solution:
             s[num] = max((s[d] for d in s if num % d == 0), key=len) | {num}
 
         return list(max(s.values(), key=len))
+
+    def largest_divisible_subset(self, nums):
+        nums = sorted(nums)
+        temp = [0] * len(nums)
+        parent = [0] * len(nums)
+
+        m = 0
+        mi = 0
+
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(i, len(nums)):
+                if nums[j] % nums[i] == 0 and temp[i] < temp[j] + 1:
+                    temp[i] = temp[j] + 1
+                    parent[i] = j
+                    if temp[i] > m:
+                        m = temp[i]
+                        mi = i
+
+        res = []
+        for i in range(m):
+            res.append(nums[mi])
+            mi = parent[mi]
+
+        return res
