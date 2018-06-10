507a4c6aed50b00038d4b2fdad66ad2d93e330f7
thinkreed <thinkreed2017@outlook.com>
2018-06-11 07:06:48 +0800

    add problem 139 world break

---
+++ algo/first/p139_word_break.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+                    break
+
+        return dp[-1]
