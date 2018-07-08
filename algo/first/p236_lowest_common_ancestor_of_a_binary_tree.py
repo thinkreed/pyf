ede84cf27698c27bfbba0aa8544edcf5d8f2dff5
thinkreed <thinkreed2017@outlook.com>
2018-07-08 16:01:26 +0800

    add problem 236 lowest common ancestor of a binary tree

---
+++ algo/first/p236_lowest_common_ancestor_of_a_binary_tree.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if root in (None, p, q):
+            return root
+
+        l, r = (self.lowestCommonAncestor(child, p, q) for child in (root.left, root.right))
+        return root if l and r else l or r
