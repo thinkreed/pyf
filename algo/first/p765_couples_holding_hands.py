1b8399b9b858c7036ef4696e2e8bb285adf9451d
thinkreed <thinkreed2017@outlook.com>
2018-09-15 00:01:56 +0800

    amend problem 765 couples holding hands

--- algo/first/p765_couples_holding_hands.py
+++ algo/first/p765_couples_holding_hands.py
@@ -12,7 +12,7 @@ class Solution(object):
             pos[temp] = y
             pos[row[y]] = x
             row[x] = row[y]
-            row[y] = 
+            row[y] = temp
 
         for i in range(n):
             pos[row[i]] = i
@@ -22,3 +22,7 @@ class Solution(object):
         for i in range(0, n, 2):
             j = row[i] + 1 if row[i] % 2 == 0 else row[i] - 1
             if row[i + 1] != j:
+                swap(i + 1, pos[j])
+                count += 1
+
+        return count
