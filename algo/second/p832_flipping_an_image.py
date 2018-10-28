d84940c507832ae8664648b0cb665eab560185c4
thinkreed <thinkreed2017@outlook.com>
2018-10-28 09:44:22 +0800

    add problem 832 flipping an image r2

---
+++ algo/second/p832_flipping_an_image.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def flipAndInvertImage(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        return [[1 ^ x for x in row[::-1]] for row in A]
