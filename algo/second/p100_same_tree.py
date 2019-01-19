31f30cd2f3e35ca298eae0a23000f05c38d75bca
thinkreed <thinkreed2017@outlook.com>
2019-01-19 20:38:07 +0800

    add problem 100 same tree r2

---
+++ algo/second/p100_same_tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+
+        if not p and not q:
+            return True
+
+        if not p and q:
+            return False
+
+        if p and not q:
+            return False
+
+        if p.val != q.val:
+            return False
+
+        if not self.isSameTree(p.left, q.left):
+            return False
+
+        if not self.isSameTree(p.righy, q.right):
+            return False
+
+        return True
