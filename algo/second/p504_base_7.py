6598dd7f18a4864d1107e842104ee0a29e37d02b
thinkreed <thinkreed2017@outlook.com>
2019-02-13 20:41:11 +0800

    add problem 504 base 7 r2

---
+++ algo/second/p504_base_7.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def convertToBase7(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        if num < 0:
+            return '-' + self.convertToBase7(-num)
+
+        if num < 7:
+            return str(num)
+
+        return self.convertToBase7(num / 7) + str(num % 7)
