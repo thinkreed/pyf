4ea7ae1d5b0e9513f912607329917405fd3e18fd
thinkreed <thinkreed2017@outlook.com>
2019-02-28 20:30:31 +0800

    add problem 232 implement queue using statcks r2

---
+++ algo/second/p232_implement_queue_with_stacks.py
@@ -0,0 +1,51 @@
+class MyQueue(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.a = []
+        self.b = []
+
+    def push(self, x):
+        """
+        Push element x to the back of queue.
+        :type x: int
+        :rtype: None
+        """
+        self.a.append(x)
+
+    def pop(self):
+        """
+        Removes the element from in front of queue and returns that element.
+        :rtype: int
+        """
+        self.helper()
+        self.b.pop()
+
+    def peek(self):
+        """
+        Get the front element.
+        :rtype: int
+        """
+        self.helper()
+        return self.b[-1]
+
+    def empty(self):
+        """
+        Returns whether the queue is empty.
+        :rtype: bool
+        """
+        return not self.a and not self.b
+
+    def helper(self):
+        if not self.b:
+            while self.a:
+                self.b.append(self.a.pop())
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
