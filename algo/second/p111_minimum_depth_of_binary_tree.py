d79c483c903e116c69bf108a4910b558a596251a
thinkreed <thinkreed2017@outlook.com>
2019-04-04 07:34:39 +0800

    add problem 111 minimum depth of binary tree r2

---
+++ algo/second/p111_minimum_depth_of_binary_tree.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+
+        q = [root]
+
+        res = 1
+
+        while q:
+            for i in range(len(q)):
+                node = q.pop(0)
+                
+                if node.left:
+                    q.append(node.left)
+
+                if node.right:
+                    q.append(node.right)
+
+                if not node.left and not node.right:
+                    return res
+
+            res += 1
+
+        return res
