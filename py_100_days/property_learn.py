f15c10dcc68479a640b9d2acae3de95bccea9b9e
thinkreed <thinkreed2017@outlook.com>
2019-06-04 23:29:10 +0800

    add day 9 oop 2 learn

---
+++ py_100_days/property_learn.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: ReedThrone
+@license: (C) Copyright 2013-2019, Reed Throne Corporation Limited.
+@contact: thinkreed2017@outlook.com
+@software: ReedThrone
+@file: property_learn.py
+@time: 2019-06-04 23:19
+@desc:
+'''
+class Person(object):
+
+    def __init__(self, name, age):
+        self._name = name
+        self._age = age
+
+    # 访问器 - getter方法
+    @property
+    def name(self):
+        return self._name
+
+    # 访问器 - getter方法
+    @property
+    def age(self):
+        return self._age
+
+    # 修改器 - setter方法
+    @age.setter
+    def age(self, age):
+        self._age = age
+
+    def play(self):
+        if self._age <= 16:
+            print('%s正在玩飞行棋.' % self._name)
+        else:
+            print('%s正在玩斗地主.' % self._name)
+
+
+def main():
+    person = Person('王大锤', 12)
+    person.play()
+    person.age = 22
+    person.play()
+    # person.name = '白元芳'  # AttributeError: can't set attribute
+
+
+if __name__ == '__main__':
+    main()
