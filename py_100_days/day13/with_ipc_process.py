fafc4e0c883f33a05bc1f51d7c0285a6ddeb1d16
thinkreed <thinkreed2017@outlook.com>
2019-06-09 22:40:12 +0800

    add day 13 multiprocess and multithread

---
+++ py_100_days/day13/with_ipc_process.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from multiprocessing import Process
+from time import sleep
+
+counter = 0
+
+
+def sub_task(string):
+    global counter
+    while counter < 10:
+        print(string, end='', flush=True)
+        counter += 1
+        sleep(0.01)
+
+
+def main():
+    Process(target=sub_task, args=('Ping',)).start()
+    Process(target=sub_task, args=('Pong',)).start()
+
+
+if __name__ == '__main__':
+    main()
