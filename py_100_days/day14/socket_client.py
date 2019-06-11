0d167abd521fea0cf3241a8d40ddfe2bef77aab7
thinkreed <thinkreed2017@outlook.com>
2019-06-11 09:21:10 +0800

    day 14 socket server and client

---
+++ py_100_days/day14/socket_client.py
@@ -0,0 +1,15 @@
+from socket import socket
+
+
+def main():
+    # 1.创建套接字对象默认使用IPv4和TCP协议
+    client = socket()
+    # 2.连接到服务器(需要指定IP地址和端口)
+    client.connect(('127.0.0.1', 6789))
+    # 3.从服务器接收数据
+    print(client.recv(1024).decode('utf-8'))
+    client.close()
+
+
+if __name__ == '__main__':
+    main()
